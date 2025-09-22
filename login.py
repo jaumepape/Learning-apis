aa042c4b06c3d2814f34d0104a2b00b05b3492eb
jaumepape <jaume@vdata.dev>
2025-09-22 11:32:50 +0200

    Obtención de JWT.

--- login.py
+++ login.py
@@ -4,8 +4,16 @@ def validate_user (user_name, password):
     user_data = dict (UserName = user_name, Password = password)
     auth_url = "https://heroes.vdata.net/api/Authentication/Authenticate"
     response = requests.post (auth_url, json = user_data)
-    return response.text
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
 user_name = input ("Ingrese su usuario: ")
 password = input ("Ingrese su contraseña: ")
 
-print (validate_user(user_name, password))
+if validate_user(user_name, password) is not None:
+    print ("Bienvenido al sistema")
+    print (validate_user(user_name, password))
+else:
+    print ("Credenciales incorrectas")
